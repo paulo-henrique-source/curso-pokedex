177ca5a58c326073cdd395860291120eb859ec8a
paulo.almeida <paulo.almeida@mblabs.com.br>
2023-03-07 11:47:11 -0300

    projeto completo

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -42,7 +42,7 @@ function Home() {
                         setPokemonInfo((prevState) =>
 
                             [...prevState, result].sort((a, b) => {
-                                return a.id - b.yaid
+                                return a.id - b.id
                             })
                         )
                     })
